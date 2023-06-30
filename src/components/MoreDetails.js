f25794a90e389d5aaee6a5470a81fda5b6278ffe
Anjum shaikh <117023049+dare-to-lead@users.noreply.github.com>
2023-06-30 12:28:24 +0530

    Update MoreDetails.js

--- src/components/MoreDetails.js
+++ src/components/MoreDetails.js
@@ -88,7 +88,7 @@ const MoreDetails = () => {
 
           <span
             style={{
-              wordSpacing: "22px",
+              wordSpacing: "10px",
             }}>
             0-2 3-5 6-8 9-11 11+
           </span>
